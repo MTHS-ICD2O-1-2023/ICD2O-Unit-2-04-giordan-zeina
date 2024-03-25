e39f41bcbee8b3f16a4f31b83961ce80183978b0
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-25 18:26:53 +0000

    math

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle () {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const calculateAreaOfTriangle = baseOfTriangle * heightOfTriangle * 0.5
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + baseOfTriangle + heightOfTriangle + ' cm²'
+}
